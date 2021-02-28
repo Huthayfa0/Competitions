23f419a82b9adc7d643f36eba82d66a7fe26d712
Huthayfa0 <huthifa0@gmail.com>
2021-02-28 16:57:56 +0200

    code2

---
+++ src/Round13/ProblemC.java
@@ -0,0 +1,38 @@
+package Round13;
+
+import java.util.Scanner;
+
+public class ProblemC {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int T= scan.nextInt();
+        while (T-->0){
+            int n= scan.nextInt();
+            long[] trampolines=new long[n];
+            for (int i=0;i<n;i++)
+                trampolines[i]= scan.nextLong();
+            System.out.println(solve(trampolines));
+        }
+    }
+
+    private static long solve(long[] trampolines) {
+        int count=0;
+        for (int i=0;i<trampolines.length;){
+            if (trampolines[i]==1){
+                i++;
+                continue;
+            }
+            count++;
+            long current=trampolines[i];
+            trampolines[i]--;
+            int j=i;
+            while (current+j<trampolines.length){
+                j= (int)(current + j);
+                current=trampolines[j];
+                if (trampolines[j]!=1)
+                    trampolines[j]--;
+            }
+        }
+        return count;
+    }
+}
