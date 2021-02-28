8135dfc39e1c4503f363698b46c1a57f697b1855
Huthayfa0 <huthifa0@gmail.com>
2021-02-28 15:28:46 +0200

    Initial commit

---
+++ src/Round13/ProblemA.java
@@ -0,0 +1,10 @@
+package Round13;
+
+import java.util.LinkedList;
+
+public class ProblemA {
+    public static void main(String[] args) {
+        LinkedList<String>a=new LinkedList<>();
+        a.subList(1,100);
+    }
+}
