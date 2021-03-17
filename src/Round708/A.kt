0cea6b6a1d7137e61146c73b244c28e924a5b5ad
Huthayfa0 <huthifa0@gmail.com>
2021-03-17 16:59:05 +0200

    A 708

--- src/Round708/A.kt
+++ src/Round708/A.kt
@@ -1,5 +1,20 @@
 package Round708
 
 fun main(){
-
+    val t= readLine()!!.toInt()
+    repeat(t){
+        val n= readLine()!!.toInt()
+        val arr= readLine()!!.split(" ").map { it.toInt() }
+        val x=IntArray(101){0}
+        arr.forEach { x[it]++ }
+        for (i in 0..100)
+            if (x[i]>0)
+                print("$i ")
+        for (i in 0..100)
+            while (x[i]>1){
+                print("$i ")
+                x[i]--
+            }
+        println()
+    }
 }
