7c0508ba281d48cd2c69289193f65b500d6c6b82
Huthayfa0 <huthifa0@gmail.com>
2021-03-18 17:05:10 +0200

    Edu round 106 A

--- src/EduRound106/A.kt
+++ src/EduRound106/A.kt
@@ -1,8 +1,16 @@
 package EduRound106
 
+import java.lang.Integer.max
+import kotlin.math.abs
+import kotlin.math.min
+
 fun main(){
     val t= readLine()!!.toInt()
     repeat(t){
-
+        val (n,k1,k2)= readLine()!!.split(' ').map { it.toInt() }
+        val (w,b)= readLine()!!.split(' ').map { it.toInt() }
+        val placeAbleW= min(k1,k2)+(abs(k1-k2)/2)
+        val placeAbleB= n-max(k1,k2)+(abs(k1-k2)/2)
+        println(if (placeAbleB>=b &&placeAbleW>=w)"Yes" else "No" )
     }
 }
