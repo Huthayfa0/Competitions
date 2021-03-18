f312454e7232ee1b4e755f32201ec4414004c823
Huthayfa0 <huthifa0@gmail.com>
2021-03-18 17:42:58 +0200

    Edu round 106 C

--- src/EduRound106/B.kt
+++ src/EduRound106/B.kt
@@ -3,6 +3,6 @@ package EduRound106
 fun main(){
     val t= readLine()!!.toInt()
     repeat(t){
-
+        val n= readLine()!!
     }
 }
--- src/EduRound106/C.kt
+++ src/EduRound106/C.kt
@@ -1,8 +1,29 @@
 package EduRound106
 
+
 fun main(){
     val t= readLine()!!.toInt()
     repeat(t){
+        val n= readLine()!!.toInt()
+        val arr= readLine()!!.split(" ").map { it.toLong() }
+        val ups=LongArray((n+1)/2)
+        val rights=LongArray(n/2)
+        for (i in 0 until n/2 ){
+            ups[i]=arr[i*2]
+            rights[i]=arr[i*2+1]
+        }
+        if (n%2==1) ups[n/2]=arr[n-1]
+        val upMin=min(ups)
+        val upRight=min(rights)
 
     }
+}
+
+fun min(arr: LongArray): Pair<Long,Int> {
+    var a=Pair(Long.MAX_VALUE,-1)
+    for (i in arr.indices)
+        if (arr[i]<a.first)
+            a=Pair(arr[i],i)
+    return a
+
 }
