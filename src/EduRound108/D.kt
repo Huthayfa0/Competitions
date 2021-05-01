f3f7300e4e12abfbe5c45c1ae6640ed2f64adb0c
Huthayfa0 <huthifa0@gmail.com>
2021-05-01 14:07:38 +0300

    Edu round 108 D

--- src/EduRound108/D.kt
+++ src/EduRound108/D.kt
@@ -1,34 +1,35 @@
 package EduRound108
 
 import java.math.BigInteger
+import kotlin.math.max
 
 fun main() {
     val n= readLine()!!.toInt()
     val change= readLine()!!.split(" ").map { it.toLong() }
     val arr=readLine()!!.split(" ").map { it.toLong() }
-    println(solve(change,arr).max(solve(change.reversed(),arr.reversed())))
+    println(max(solve(change,arr),(solve(change.reversed(),arr.reversed()))))
 }
 
-fun solve(change: List<Long>, arr: List<Long>): BigInteger {
- val fixed=Array<BigInteger>( arr.size){ BigInteger.ZERO}
-    fixed[0]= BigInteger.valueOf(change[0]).multiply(BigInteger.valueOf(arr[0]))
+fun solve(change: List<Long>, arr: List<Long>): Long {
+ val fixed=LongArray( arr.size)
+    fixed[0]= change[0]*arr[0]
  for (i in 1 until arr.size){
-     fixed[i]=BigInteger.valueOf(change[i]).multiply(BigInteger.valueOf(arr[i])).add(fixed[i-1])
+     fixed[i]=change[i]*arr[i]+fixed[i-1]
  }
     var maximum=fixed[arr.size-1]
     val after=Array(arr.size){
-        val x=Array<BigInteger>( it+1){ BigInteger.ZERO}
-        x[0]= BigInteger.valueOf(change[it]).multiply(BigInteger.valueOf(arr[0]))
+        val x=LongArray( it+1)
+        x[0]= change[it]*arr[0]
         for (i in 1..it){
-            x[i]=BigInteger.valueOf(change[it-i]).multiply(BigInteger.valueOf(arr[i])).add(x[i-1])
+            x[i]=change[it-i]*arr[i]+x[i-1]
         }
         return@Array x
     }
     after.forEach {
         for (i in 0 until it.size/2){
-            val ch=fixed[fixed.size-1].subtract(fixed[it.size-i-1]).add(if (i!=0)fixed[i-1]else  BigInteger.ZERO)
-                .add(it[it.size-i-1]).subtract(if (i!=0||it.size-i-1==i)it[i-1]else BigInteger.ZERO)
-            maximum=maximum.max(ch)
+            val ch=fixed[fixed.size-1]-fixed[it.size-i-1]+(if (i!=0)fixed[i-1]else  0L)+
+                it[it.size-i-1]-(if (i==0||it.size-i-1==i)0L else it[i-1])
+            maximum= max(ch,maximum)
         }
     }
     return maximum
