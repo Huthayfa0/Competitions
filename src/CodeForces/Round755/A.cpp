002c03d8933f88227ed2e91fb395042e6d4d84e5
Huthayfa0 <huthifa0@gmail.com>
2021-11-14 08:18:21 +0200

    Round755

---
+++ src/CodeForces/Round755/A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> VL;
+typedef vector<VL> ML;
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define lp(i,s,f) for(ll i = s; i < ll(f); i++)
+#define inF freopen("in", "r", stdin);
+#define outF freopen("out", "w", stdout);
+#define endl '\n'
+#define MOD 1000000000
+#define mm(arr) memset(arr, 0, sizeof(arr))
+#define sz(v) (int) v.size()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+void chmin(ll&a,ll b){if(a>b)a=b;}
+void chmax(ll&a,ll b){if(a<b)a=b;}
+//DepthFirstSearch BreadthFirstSearch DijkstraShortestPath
+//FloydWarshallShortestPath BellmanFordShortestPath PrimMST
+//KruskalMST PrimeCheck Factorization NumberPower Matrix
+
+void solve(){
+    ll u,v;cin>>u>>v;
+    cout<<-u*u<<" "<<v*v<<endl;
+}
+
+int main(){
+    FAST
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
