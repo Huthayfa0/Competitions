966c57f90d640d289d8f1ddc8ed22a25efaca434
Huthayfa0 <huthifa0@gmail.com>
2021-12-25 14:09:14 +0200

    BeginnerContest233

---
+++ src/AtCoder/BeginnerContest233/B.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const long double PI = acos(-1);
+typedef vector<ll> VL;
+typedef vector<VL> ML;
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define lp(i,s,f) for(ll i = s; (i) < ll(f); (i)++)
+#define re(i,a) lp(i,0,sz(a)) cin >> (a)[i]
+#define out(i,a) lp(i,0,sz(a)) cout << (a)[i] << ((i) == sz(a) - 1 ? '\n' : ' ')
+#define inF freopen("in", "r", stdin);
+#define outF freopen("out", "w", stdout);
+#define endl '\n'
+#define MOD 100000007
+#define mm(arr) memset(arr, 0, sizeof(arr))
+#define sz(v) (int) (v).size()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+#define decimalpoint cout << std::fixed << setprecision(5)
+void chmin(ll&a,ll b){if(a>b)a=b;}
+void chmax(ll&a,ll b){if(a<b)a=b;}
+//DepthFirstSearch BreadthFirstSearch DijkstraShortestPath
+//FloydWarshallShortestPath BellmanFordShortestPath PrimMST
+//KruskalMST PrimeCheck Factorization NumberPower Matrix
+
+void solve(){
+    ll x,y;cin>>x>>y;
+    string s;
+    x--;
+    cin>>s;
+    lp(i,0,x){
+        cout<<s[i];
+    }
+    lp(i,0,y-x){
+        cout<<s[y-i-1];
+    }
+    lp(i,y,s.length()){
+        cout<<s[i];
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    FAST
+    ll t;
+    t=1;
+    while(t--){
+       solve();
+    }
+    return 0;
+}
