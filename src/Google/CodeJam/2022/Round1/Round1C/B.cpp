621c9245a40e51f682d3a9ec81c205b8da329aa8
Huthayfa0 <huthifa0@gmail.com>
2022-04-30 13:42:57 +0300

    Round1C

---
+++ src/Google/CodeJam/2022/Round1/Round1C/B.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> VL;
+typedef vector<VL> ML;
+const double PI = acos(-1.0);
+const ld EPS = 1e-18;
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define lp(i,s,f) for(ll i = s; (i) < ll(f); (i)++)
+#define re(i,a) lp(i,0,sz(a)) cin >> (a)[i]
+#define out(i,a) lp(i,0,sz(a)) cout << (a)[i] << ((i) == sz(a) - 1 ? '\n' : ' ')
+#define inF freopen("in", "r", stdin);
+#define outF freopen("out", "w", stdout);
+#define endl '\n'
+#define MOD 1000000007
+#define inf 0x3f3f3f3f3f3f3f3fLL
+#define mm(arr) memset(arr, 0, sizeof(arr))
+#define sz(v) (int) (v).size()
+#define all(v) (v).begin(), (v).end()
+#define F first
+#define S second
+#define decimalpoint cout << std::fixed << setprecision(2)
+typedef complex<ld> point;
+
+using namespace __gnu_pbds;
+#define ordered_set tree<pair<ll,ll>, null_type,less<>, rb_tree_tag,tree_order_statistics_node_update>
+void chmin(ll&a,ll b){if(a>b)a=b;}
+void chmax(ll&a,ll b){if(a<b)a=b;}
+//DepthFirstSearch BreadthFirstSearch DijkstraShortestPath
+//FloydWarshallShortestPath PrimMST MatrixExponentiation
+//KruskalMST PrimeCheck Factorization NumberPower
+//DivisionInverse Combinations Geometry
+//LinesGeometry CirclesGeometry
+
+void solve(){
+    ll n,k;cin>>n>>k;
+    VL arr(n);re(i,arr);
+    ll sum=arr[0];
+    ll sum_ab=0;
+    lp(i,1,n){
+        sum_ab+=arr[i]*sum;
+        sum+=arr[i];
+    }
+    if(k==1){
+    if(sum!=0&&sum_ab%sum==0){
+        cout<<-sum_ab/sum<<endl;
+    }else if(sum==sum_ab&&sum==0){
+        cout<<0<<endl;
+    }else {
+        cout << "IMPOSSIBLE" << endl;
+    }
+    }else{
+        cout<<1-sum<<" ";
+        sum_ab+=(1-sum)*sum;
+        cout<<-sum_ab;
+        lp(i,2,k){
+            cout<<" 0";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    FAST
+    ll t=1;
+    cin>>t;
+    ll nn=t;
+    while(t--){
+       cout<<"Case #"<<nn-t<<": ";//<<endl;
+        solve();
+    }
+    return 0;
+}
